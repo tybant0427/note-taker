e291cbfea2a80e378c60bc5a740c0b5484bb94e9
Tyler Bantley <tyler.bantley@icloud.com>
2022-01-23 18:47:12 -0600

    HTML calls

--- server.js
+++ server.js
@@ -9,3 +9,18 @@ const PORT = 3005;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+//HTML calls
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+app.get('/index', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
+  //listening on
+  app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+  });
